1e20b2501b4dc601d8955b5f666a4b5bc6326bab
Peter Termaten <peter.termaten@gmail.com>
2015-01-12 22:53:00 +0100

    moved TIS bundle to src/AppBundle

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     *
+     * @param $name
+     * @return array
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+}
---
+++ src/AppBundle/Utils/Tester.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TIS\AppBundle\Utils;
+
+class Tester
+{
+    public function MyTest1()
+    {
+        return 'This is a teststring1';
+    }
+}
