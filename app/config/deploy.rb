4d5ed4223aa93a8337a526c226119ed90a7e330b
Peter Termaten <peter.termaten@gmail.com>
2015-02-11 17:08:46 +0100

    modified deploy.rb for Capifony; added web/assets to repo

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,9 +1,9 @@
 set :application, "Bach"
-set :domain,      "vps001.teris.nl"
+set :domain,      "vps002.teris.nl"
 set :deploy_to,   "/var/www/html/jsb.termaten.eu"
 set :app_path,    "app"
 
-set :repository,  "https://github.com/P3t3rT/bach.git"
+set :repository,  "git@github.com:P3t3rT/bach.git"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
@@ -16,9 +16,10 @@ role :app,        domain, :primary => true       # This may be the same as your 
 set  :keep_releases,  3
 
 # Be more verbose by uncommenting the following line
-#logger.level = Logger::MAX_LEVEL
+logger.level = Logger::MAX_LEVEL
 
 set :dump_assetic_assets, true
+set :composer_bin, "composer.phar"  #PT
 set :use_composer, true
 set :composer_options,  "--no-dev --verbose --prefer-source --optimize-autoloader"   #PT
 #set :install_vendors, true                        #PT = default
@@ -30,10 +31,9 @@ set :writable_dirs, ["app/cache", "app/logs"]
 set :webserver_user, "www-data"
 set :permission_method, :chown
 set :use_set_permissions, true
-set :use_sudo, true                              #PT
+set :use_sudo, false                              #PT
 set :user, "root"                                #PT
 
 ssh_options[:forward_agent] = true
 ssh_options[:port] = "65022"                     #PT
 default_run_options[:pty] = true
-
