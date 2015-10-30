ce4f00a89c154f64ceefcf7a8dbd08d106169089
Peter Termaten <peter.termaten@gmail.com>
2015-10-30 15:00:32 +0100

    update deploy script, added Apache restart and Memcached flush

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -15,7 +15,7 @@ role :app,        domain, :primary => true       # This may be the same as your 
 set  :keep_releases,  3
 
 # Be more verbose by uncommenting the following line
-#logger.level = Logger::MAX_LEVEL
+# logger.level = Logger::MAX_LEVEL
 
 set :dump_assetic_assets, true
 set :composer_bin, "composer.phar"                 #PT
@@ -36,3 +36,20 @@ set :user, "root"                                #PT
 ssh_options[:forward_agent] = true
 ssh_options[:port] = "65022"                     #PT
 default_run_options[:pty] = true
+
+# Custom tasks
+namespace :deploy do
+	# Apache needs to be restarted to make sure that the APC cache is cleared.
+	# Memcached will be flushed to clear the memcached cache
+	# This overwrites the :restart task in the parent config which is empty.
+	desc "Restart Apache"
+	task :restart, :except => { :no_release => true }, :roles => :app do
+		run "echo 'flush_all' | nc localhost 11211"
+		puts "--> Memcached successfully flushed".green
+		run "sudo service apache2 restart"
+        puts "--> Apache successfully restarted".green
+	end
+end
+
+
+
