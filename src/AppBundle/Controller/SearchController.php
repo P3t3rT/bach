e387e8713a061ab0f384422923f78e501140c2aa
Peter Termaten <peter.termaten@gmail.com>
2015-11-13 23:26:59 +0100

    SearchController - 1

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -15,6 +15,19 @@ use Symfony\Component\HttpFoundation\Response;
 class SearchController extends Controller
 {
     /**
+     * @var Raw search results
+     */
+    private $results;
+    /**
+     * @var Raw search results transformed
+     */
+    private $transformed;
+    /**
+     * @var hit data from search result
+     */
+    private $hit;
+
+    /**
      * @Route("/search", name="bach_search")
      *
      */
@@ -42,21 +55,9 @@ class SearchController extends Controller
         ));
 
         // Returns a mixed array of any objects mapped + highlights
-        $results = $finder->findHybrid($query);
+        $this->results = $finder->findHybrid($query);
 
-        $rows = array();
-        $i=0;
-        foreach ($results as $result) {
-            $hit = $result->getResult()->getHit();
-            $rows[$i]['type'] = $hit['_type'];
-            foreach ($hit['highlight'] as $field => $highlights) {
-                $tmp1 = $field;
-                foreach ($highlights as $highlight) {
-                    $rows[$i]['highlight'] = $highlight;
-                }
-            }
-        $i++;
-        }
+        $rows = $this->_processResults();
 
         return $this->render('search_results.html.twig', array('rows' => $rows));
 
@@ -81,6 +82,56 @@ class SearchController extends Controller
           }
         }
     */
+
+    }
+
+    protected function _processResults()
+    {
+        $rows = array();
+        $i=0;
+        foreach ($this->results as $result) {
+           $this->transformed = $result->getTransformed();
+           $this->hit = $result->getResult()->getHit();
+            switch ($this->hit['_type']) {
+                case "opus":
+                    $this->getOpusData($i);
+                    break;
+                case "part":
+                    $this->getPartData($i);
+                    break;
+                case "audiotrack":
+                    $this->getAudiotrackData($i);
+            }
+
+
+
+//           $rows[$i]['type'] = $hit['_type'];
+//           foreach ($hit['highlight'] as $field => $highlights) {
+//               $tmp1 = $field;
+//               foreach ($highlights as $highlight) {
+//                   $rows[$i]['highlight'] = $highlight;
+//               }
+//           }
+        $i++;
+        }
+
+        return $rows;
+    }
+
+    protected function getOpusData($i)
+    {
+        $rows[$i]['opus']['id'] = $this->transformed->getId();
+        $rows[$i]['opus']['opus'] = $this->transformed->getOpus();
+        $rows[$i]['opus']['title'] = $this->hit['highlight']['opus_title'][0];
+    }
+
+    protected function getPartData($i)
+    {
+
+    }
+
+    protected function getAudiotrackData($i)
+    {
 
     }
 }
